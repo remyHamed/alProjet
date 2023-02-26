d19c5ff3d8725e625e5d3c6fe5051ced68730f69
crichardd <crichard11@myges.fr>
2023-02-26 23:57:09 +0100

    controller offre

--- src/main/java/fr/alProject/domain/enumerator/StatutOffer.java
+++ src/main/java/fr/alProject/domain/enumerator/StatutOffer.java
@@ -1,14 +1,7 @@
 package fr.alProject.domain.enumerator;
 
-
 public enum StatutOffer {
-    
-    EN_ATTENTE(1),EN_COURS(2),TERMINEE(3);
-
-    private final int statusNbOffre;
-
-    StatutOffer(int statusNbOffre) {
-        this.statusNbOffre = statusNbOffre;
-    }
-    
+    EN_ATTENTE,
+    EN_COURS,
+    TERMINEE
 }
