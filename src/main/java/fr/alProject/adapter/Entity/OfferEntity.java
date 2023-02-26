950a0e97ad1792556df9b096ea78315e26b3b319
crichardd <crichard11@myges.fr>
2023-02-26 23:53:06 +0100

    Offre entity et controller

--- src/main/java/fr/alProject/adapter/Entity/OfferEntity.java
+++ src/main/java/fr/alProject/adapter/Entity/OfferEntity.java
@@ -1,5 +1,34 @@
 package fr.alProject.adapter.Entity;
 
+import fr.alProject.domain.Model.Offer;
+import lombok.Getter;
+import lombok.Setter;import fr.alProject.domain.enumerator.StatutOffer;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class OfferEntity {
-    
+
+    private Long id;
+    private String title;
+    private String description;
+    private BigDecimal price;
+    private LocalDate deadline;
+    private StatutOffer statutOffer;
+
+    public OfferEntity(String title, String description, BigDecimal price, LocalDate deadline, StatutOffer statutOffer) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.deadline = deadline;
+        this.statutOffer = statutOffer;
+    }
+
+    public OfferEntity() {
+
+    }
+
 }
+
